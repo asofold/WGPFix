13b015ce410562c37bde9baa7e81945667b72812
asofold <asofold@web.de>
2012-06-29 15:52:19 +0200

    new version of compatlayer

--- WorldGuardPistonFix/src/asofold/fix/wgp/compatlayer/CompatConfig.java
+++ WorldGuardPistonFix/src/asofold/fix/wgp/compatlayer/CompatConfig.java
@@ -3,6 +3,7 @@ package asofold.fix.wgp.compatlayer;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * CONVENTIONS: 
@@ -15,33 +16,7 @@ import java.util.Map;
  */
 public interface CompatConfig {
 	
-	public boolean hasEntry(String path);
-
-	public void load();
-	
-	public boolean save();
-
-	public Double getDouble(String path, Double defaultValue);
-
-	public Long getLong(String path, Long defaultValue);
-
-	public String getString(String path, String defaultValue);
-
-	public Integer getInt(String path, Integer defaultValue);
-	
-	public List<String> getStringKeys(String path);
-	
-	public List<Object> getKeys(String path);
-	public List<Object> getKeys();
-
-	public Object getProperty(String path, Object defaultValue);
-
-	public List<String> getStringKeys();
-
-	public void setProperty(String path, Object obj);
-
-	public List<String> getStringList(String path, List<String> defaultValue);
-	
+	// Boolean
 	/**
 	 * Only accepts true and false , 'true' and 'false'.
 	 * @param path
@@ -49,17 +24,132 @@ public interface CompatConfig {
 	 * @return
 	 */
 	public Boolean getBoolean(String path, Boolean defaultValue);
+	public Boolean getBoolean(String path);
 	
-	public List<Integer> getIntList(String path, List<Integer> defaultValue);
+	// Long
+	public Long getLong(String path);
+	public Long getLong(String path, Long defaultValue);
 	
-	public void removeProperty(String path);
-	
+	// Double
+	public Double getDouble(String path);
+	public Double getDouble(String path, Double defaultValue);
+	public List<Double> getDoubleList(String path);
 	public List<Double> getDoubleList(String path , List<Double> defaultValue);
 	
+	// Integer (abbreviation)
+	public Integer getInt(String path);
+	public Integer getInt(String path, Integer defaultValue);
+	public List<Integer> getIntList(String path);
+	public List<Integer> getIntList(String path, List<Integer> defaultValue);
+	// Integer (full name)
+	public Integer getInteger(String path);
+	public Integer getInteger(String path, Integer defaultValue);
+	public List<Integer> getIntegerList(String path);
+	public List<Integer> getIntegerList(String path, List<Integer> defaultValue);
+
+	// String
+	public String getString(String path);
+	public String getString(String path, String defaultValue);
+	public List<String> getStringList(String path);
+	public List<String> getStringList(String path, List<String> defaultValue);
+	
+	// Generic methods:
+	public Object get(String path);
+	public Object get(String path, Object defaultValue);
+	public Object getProperty(String path);
+	public Object getProperty(String path, Object defaultValue);
+	public void set(String path, Object value);
+	public void setProperty(String path, Object value);
+	
+	/**
+	 * Remove a path (would also remove sub sections, unless for path naming problems).
+	 * @param path
+	 */
+	public void remove(String path);
+	
+	/**
+	 * Works same as remove(path): removes properties and sections alike.
+	 * @param path
+	 */
+	public void removeProperty(String path);
+	
+	// Contains/has
+	public boolean hasEntry(String path);
+	public boolean contains(String path);
+	
+	// Keys (Object): [possibly deprecated]
+	/**
+	 * @deprecated Seems not to be supported anymore by new configuration, use getStringKeys(String path);
+	 * @param path
+	 * @return
+	 */
+	public List<Object> getKeys(String path);
+	/**
+	 * @deprecated Seems not to be supported anymore by new configuration, use getStringKeys();
+	 * @return
+	 */
+	public List<Object> getKeys();
+	
+	// Keys (String):
+	/**
+	 * 
+	 * @return never null
+	 */
+	public List<String> getStringKeys();
+	
+	public List<String> getStringKeys(String path);
+	
+	/**
+	 * Get all keys from the section (deep or shallow).
+	 * @param path
+	 * @param deep
+	 * @return Never null.
+	 */
+	public Set<String> getStringKeys(String path, boolean deep);
+	
+	/**
+	 * convenience method.
+	 * @param path
+	 * @return never null
+	 * 
+	 */
+	public Set<String> getStringKeysDeep(String path);
+	
+	// Values:
 	/**
 	 * Equivalent to new config: values(true)
 	 * @return
 	 */
 	public Map<String, Object> getValuesDeep();
 	
+	// Technical / IO:
+	/**
+	 * False if not supported.
+	 * @param sep
+	 * @return
+	 */
+	public boolean setPathSeparatorChar(char sep);
+	
+	public void load();
+	
+	public boolean save();
+	
+	/**
+	 * Clear all contents.
+	 */
+	public void clear();
+
+	/**
+	 * Return a YAML-String representation of the contents, null if not supported.
+	 * @return null if not supported.
+	 */
+	public String getYAMLString();
+	
+	/**
+	 * Add all entries from the YAML-String representation to the configuration, forget or clear all previous entries. 
+	 * @return
+	 */
+	public boolean fromYamlString(String input);
+	
+
 }
--- WorldGuardPistonFix/src/asofold/fix/wgp/compatlayer/CompatConfigFactory.java
+++ WorldGuardPistonFix/src/asofold/fix/wgp/compatlayer/CompatConfigFactory.java
@@ -4,21 +4,22 @@ import java.io.File;
 
 public class CompatConfigFactory {
 	
-	public static final String version = "0.2.0";
+	public static final String version = "0.8.1";
 	
 	/**
-	 * Attempt to get a working file configuration.
-	 * Use getDBConfig to use this with a database.
-	 * @param file
+	 * Attempt to get a working file configuration.<br>
+	 * This is not fit for fast processing.<br>
+	 * Use getDBConfig to use this with a database.<br>
+	 * @param file May be null (then memory is used).
 	 * @return null if fails.
 	 */
 	public static final CompatConfig getConfig(File file){
 		CompatConfig out = null;
 		// TODO: add more (latest API)
-		try{
-			return new OldConfig(file);
-		} catch (Throwable t){
-		}
+//		try{
+//			return new OldConfig(file);
+//		} catch (Throwable t){
+//		}
 		try{
 			return new NewConfig(file);
 		} catch (Throwable t){
@@ -27,9 +28,9 @@ public class CompatConfigFactory {
 		return out;
 	}
 	
-	public static final CompatConfig getOldConfig(File file){
-		return new OldConfig(file);
-	}
+//	public static final CompatConfig getOldConfig(File file){
+//		return new OldConfig(file);
+//	}
 	
 	public static final CompatConfig getNewConfig(File file){
 		return new NewConfig(file);
